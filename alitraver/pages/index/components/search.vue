3c3c09d110a93fb6f0c98fe0776523e14dae9821
Nucky <411853404@qq.com>
2020-04-22 10:12:34 +0800

    ！

---
+++ alitraver/pages/index/components/search.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<!-- 搜索 -->
+		<view>
+			<image src="../../../static/tab/saoma.svg" model="widthFix"></image>
+		</view>
+		<view>2</view>
+		<view>3</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		name:'search'
+	}
+</script>
+	
+<style>
+</style>
